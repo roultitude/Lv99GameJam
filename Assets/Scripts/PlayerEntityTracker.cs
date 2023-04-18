0fbacc44555dca1baa058c6a795c66096ca1bfcb
roultitude <51879825+roultitude@users.noreply.github.com>
2023-04-19 01:42:56 +0800

    Add Camera tracking to player

--- Assets/Scripts/PlayerEntityTracker.cs
+++ Assets/Scripts/PlayerEntityTracker.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 public class PlayerEntityTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Entity playerEntity;
+
+
+    private void LateUpdate()
     {
-        
+        if(playerEntity != Entity.Null)
+        {
+            Vector3 followPos = World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<LocalToWorld>(playerEntity).Position;
+            transform.position = followPos;
+        } else
+        {
+            playerEntity = GetPlayerEntity();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Entity GetPlayerEntity()
     {
-        
+        EntityQuery playerTagEntityQuery = World.DefaultGameObjectInjectionWorld.EntityManager.CreateEntityQuery(typeof(PlayerTag));
+        NativeArray<Entity> entityNativeArray = playerTagEntityQuery.ToEntityArray(Unity.Collections.Allocator.Temp);
+
+        if(entityNativeArray.Length > 0)
+        {
+            return entityNativeArray[0];
+        }
+        return Entity.Null;
     }
 }
